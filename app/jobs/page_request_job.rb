7ae766a8d6c61099d59f78fc671e3696695e1e79
Hubert Pham <hubqwerty@berkeley.edu>
2017-04-04 00:06:08 -0700

    update

--- app/jobs/page_request_job.rb
+++ app/jobs/page_request_job.rb
@@ -4,7 +4,7 @@ class PageRequestJob < ApplicationJob
 
   def perform(url, id)
     html = HTTP.get(url).to_s
-    File.open(Rails.root.join('public', 'uploads', "#{id}.txt"), 'wb') {|f| f.write(html) }
+    File.open(Rails.root.join('public', 'uploads', "#{id}.txt"), 'w') {|f| f.write(html) }
     Job.find(id).update_attributes(:completed => true)
   end
 end
